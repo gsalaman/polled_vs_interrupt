38428b611d0cc8b2c7442e3781aa4c38c37af556
Glenn Salaman <g.salaman@comcast.net>
2021-02-25 07:33:06 -0700

    initial revision

--- polled_vs_interrupt.ino
+++ polled_vs_interrupt.ino
@@ -1,42 +1,82 @@
 /* Polling inputs vs interrupts */
 
-/* Polled example. */
+/* Single Pin change interrupt example. */
 
-#define BUTTON_PIN  3
+#define BUTTON_PIN  7
 #define LED_PIN     8
 
-int  last_button_state;
 int  press_count=0;
+
+void configure_isrs( void )
+{
+  // On the Uno, we have 3 blocks of pin change interrupts.
+  // One ISR tells us if a pin from D0 to D7 changes,
+  // another tells us if a pin from A0 to A5 changes, 
+  // and finally  the last tells us if a pin  from D8 to D13 changes.
+
+  // There are three "mask" registers, one for each of those ISRs, to tell which
+  // pins in that ISR to pay attention to.  A "one" means we care about that pin;
+  // a zero means we don't (and we won't get the corresponding ISR for a change on 
+  // that pin.
+
+  // PCMSK0 controls D8 to D13 like this:
+  // | X | X | D13 | D12 | D11| D10 | D9 | D8 |
+
+  // PCMSK1 controls A0 to A5 like this:
+  // | X | X | A5 | A4 | A3 | A2 | A1 | A0 |
+
+  // PCMSK2 controls D7  to D0 like this:
+  // | D7 | D6 | D5 | D4 | D3 | D2 | D1 | D0 |
+
+  // In this sketch, we only care about changes on pin 7, so we want to set PCMSK2
+  PCMSK2 = B10000000;
+
+  //PCIFR = B00000000;
+  
+  // PCICR is the Uno's "Pin Change Interrupt Control Register"
+  // Bit zero turns on the ISR for Pins D8 to D15
+  // Bit one  turns on the ISR for pins A0 to A5
+  // Bit two turns on the ISR for pins D0 to D7
+
+  // Since we want pin 7, we enable the ISR in bit two
+  PCICR = B00000100;
+
+  // Finally, I'm gonna do a double-check here...these registers are set for pin 7, 
+  // Which was #defined at the top of our file.  I want to make sure someone didn't 
+  // change that #define using a little pre-processor check
+  #if (BUTTON_PIN != 7)
+  #error "Someone changed BUTTON_PIN!!!"
+  #endif
+}
+
+ISR(PCINT2_vect)
+{
+  int pin7_val;
+
+  pin7_val = digitalRead(BUTTON_PIN);
+
+    press_count++;
+  
+  // the invert is because pin7 is active low
+  digitalWrite(LED_PIN, !pin7_val); 
+}
 
 void setup( void )
 {
   Serial.begin(9600);
 
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  last_button_state = HIGH;
+  configure_isrs();
 
-  Serial.println("Polled Button:  initialized");
+  Serial.println("Single pin change ISR example initialized");
 }
 
 void loop( void )
 {
-  int button_state;
-  
-  button_state = digitalRead(BUTTON_PIN);
-  
-  // Only count transitions from HIGH to LOW as presses.
-  if ((last_button_state == HIGH) && (button_state == LOW))
-  {
-    press_count++;
-    Serial.print(press_count);
-    Serial.println(" button presses");
-  }
 
-  last_button_state = button_state;
+  Serial.print("Button presses: ");
+  Serial.println(press_count);
 
-  // We want our LED on when the button is pressed, and off when
-  // it's not.  Since we've got a pull-up resistor, the line is LOW
-  // when it's pressed and HIGH when it's not.
-  digitalWrite(LED_PIN, !button_state);
+  delay(1000);
 
 }
